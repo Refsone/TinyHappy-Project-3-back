bbbe55d056d0cdfd6a4925da59a02bdcd13ff11a
Auxence <auxence.blondel@gmail.com>
2020-06-14 17:34:14 +0200

    configure .env and add first get test

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+// const connection = require('./conf')
 
-app.listen(8080, () => {
-  console.log('server is listening on port 8080')
+// Support JSON-encoded bodies
+app.use(express.json())
+// Support URL-encoded bodies
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get('/', (req, res) => {
+  res.send('First get')
+})
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log('Something bad happened...')
+  } else {
+    console.log(`server is listening on port ${process.env.PORT}`)
+  }
 })
