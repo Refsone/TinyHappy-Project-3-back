cce4a9eedae4c8d591b659371815114ce28fdd92
hipro90 <hipro90@gmail.com>
2020-06-16 10:28:29 +0200

    create route

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express')
 const app = express()
+const connexion = require('./conf.js')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
+
+app.get('/:id/moments/create', (req, res) => {
+  connexion.query('SELECT family_member.id, family_firstname, color FROM family_member JOIN user ON user.id = family_member.user_id JOIN color_family ON color_family.id = color_family_id WHERE user.id = ?', [req.params.id], (err, results) => {
+    if (err) {
+      res.send(err)
+    }res.status(200).json(results)
+  })
+})
 
 app.listen(8080, () => {
   console.log('server is listening on port 8080')
