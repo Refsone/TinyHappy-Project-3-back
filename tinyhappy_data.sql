9af7e412f552d9e4035733526fe1e094946ceb02
Auxence <auxence.blondel@gmail.com>
2020-06-17 00:00:25 +0200

    new data and structure sql

--- tinyhappy_data.sql
+++ tinyhappy_data.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `color_family` WRITE;
 /*!40000 ALTER TABLE `color_family` DISABLE KEYS */;
-INSERT INTO `color_family` VALUES (1,'#B6C5E6'),(2,'#FFB8D5'),(3,'#BDFBFE'),(4,'#E9FFC3'),(5,'#FFEDD7'),(6,'#FEFFC5'),(7,'#F6EAFF'),(8,'#DAFFD7');
+INSERT INTO `color_family` VALUES (1,'#E9FEFC'),(2,'#F2FFD7'),(3,'#E9EFFA'),(4,'#FFE7EC'),(5,'#FFEDD7'),(6,'#FEFFC5'),(7,'#F6EAFF'),(8,'#DAFFD7');
 /*!40000 ALTER TABLE `color_family` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,7 +61,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `moment` WRITE;
 /*!40000 ALTER TABLE `moment` DISABLE KEYS */;
-INSERT INTO `moment` VALUES (3,0,'“Hey Louise, arrête de faire du foin-foin là !”','En parlant à sa soeur qui est un peu bruyante.','2020-05-12',0,1,2),(13,0,'Les filles ont fabriqué une cabane avec tous les éléments mobiles de l’appart. 30 minutes de rangement, youpi.',NULL,'2020-05-09',1,1,1),(14,1,'Les filles sont chez les grands-parents !!!!Liberés, délivrés !',NULL,'2020-05-09',0,1,1),(15,0,'“Dis papa, tu connais l’histoire de la coquillette molle ?”',NULL,'2020-05-07',0,1,2),(16,0,'Elle a fait ses premiers pas !!',NULL,'2020-05-02',0,1,1);
+INSERT INTO `moment` VALUES (3,0,'“Hey Louise, arrête de faire du foin-foin là !”','En parlant à sa soeur qui est un peu bruyante.','2020-05-12',1,1,2),(13,0,'Les filles ont fabriqué une cabane avec tous les éléments mobiles de l’appart. 30 minutes de rangement, youpi.',NULL,'2020-05-09',0,1,1),(14,1,'Les filles sont chez les grands-parents !!!!Liberés, délivrés !',NULL,'2020-05-09',1,1,1),(15,0,'“Dis papa, tu connais l’histoire de la coquillette molle ?”',NULL,'2020-05-07',0,1,2),(16,0,'Elle a fait ses premiers pas !!',NULL,'2020-05-02',0,1,1),(18,0,'blabla',NULL,'2020-05-02',0,1,1);
 /*!40000 ALTER TABLE `moment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,7 +91,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Jérôme','Benoit','jeromebenoit@gmail.com','azerty',NULL,NULL,1),(6,'Jérôme ','Potié','jeromepotie@gmail','bidule',NULL,NULL,1);
+INSERT INTO `user` VALUES (1,'Jérôme','Benoit','jeromebenoit@gmail.com','azerty',NULL,NULL,1,1),(6,'Jérôme ','Potié','jeromepotie@gmail','bidule',NULL,NULL,1,NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,4 +114,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-14 22:49:13
+-- Dump completed on 2020-06-16 23:59:57
