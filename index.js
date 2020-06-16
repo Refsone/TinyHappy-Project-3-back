6152e2dd7a7369d972a9f838569bcd962fdd564e
hipro90 <hipro90@gmail.com>
2020-06-16 10:28:29 +0200

    create route

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const app = express()
+<<<<<<< HEAD
 const routes = require('./routes/index')
+=======
+const connexion = require('./conf.js')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
+
+app.get('/:id/moments/create', (req, res) => {
+  connexion.query('SELECT family_member.id, family_firstname, color FROM family_member JOIN user ON user.id = family_member.user_id JOIN color_family ON color_family.id = color_family_id WHERE user.id = ?', [req.params.id], (err, results) => {
+    if (err) {
+      res.send(err)
+    }res.status(200).json(results)
+  })
+})
+>>>>>>> cce4a9e... create route
 
 app.use(express.json())
 app.use(express.urlencoded({
