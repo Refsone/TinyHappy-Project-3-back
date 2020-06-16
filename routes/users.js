fbb17ac04646f7a57ca44d628bc7959456c47ee5
Auxence <auxence.blondel@gmail.com>
2020-06-16 14:57:13 +0200

    create express.Router for moments get

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,7 @@ const connection = require('../conf')
 router.get('/:id/moments', (req, res) => {
   connection.query('SELECT moment.id, moment_text, moment_context, moment_favorite, moment_event_date, family_firstname, color FROM moment JOIN family_moment ON moment_id=moment.id JOIN family_member fme ON family_member_id=fme.id JOIN color_family ON color_family_id=color_family.id WHERE moment.user_id = ? GROUP BY moment.id, fme.id', [req.params.id], (err, results) => {
     if (err) {
-      // res.status(500).send('Erreur lors de la récupération des moments')
-      res.status(500).send(err)
+      res.status(500).send('Erreur lors de la récupération des moments')
     } else {
       let prevText = ''
       let prevName = []
