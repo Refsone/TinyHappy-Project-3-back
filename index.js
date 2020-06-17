b4f977be213ee4d6d83559ca76c51a529e4be8c0
jerkodeur <jerome.potie@gmail.com>
2020-06-17 11:15:10 +0200

    Initialize back and create the first connexion requests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -8,11 +9,7 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept')
-//   next()
-// })
+app.use(cors('*'))
 
 app.use('/family', routes.family)
 
