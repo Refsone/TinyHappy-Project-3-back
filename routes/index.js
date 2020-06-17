b32e329d3752fa5af9a946dfa786c9d63d847d55
jerkodeur <jerome.potie@gmail.com>
2020-06-17 13:22:21 +0200

    Create a get route to fetch colors data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const users = require('./users')
 const family = require('./family')
+const colors = require('./colors')
 const moments = require('./moments')
 
 module.exports = {
   users,
   moments,
-  family
+  family,
+  colors
 }
