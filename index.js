7b8c5361fc021a9a06728ae10ab27a3342cac322
jerkodeur <jerome.potie@gmail.com>
2020-06-17 11:15:10 +0200

    Initialize back and create the first connexion requests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
@@ -10,12 +11,6 @@ app.use(express.urlencoded({
 }))
 
 app.use(cors('*'))
-
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept')
-//   next()
-// })
 
 app.use('/users', routes.users)
 app.use('/moments', routes.moments)
--- routes/family.js
+++ routes/family.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const connection = require('../conf')
+// const connection = require('../conf')
 
-router.get('/', (req, res) => {
-  const sql = 'SELECT * FROM family_member'
-  connection.query(sql, (err, results) => {
-    if (err) {
-      return res.status(404).send('Ressources not found')
-    } else {
-      return res.status(200).json(results)
-    }
-  })
+router.post('/', (req, res) => {
+  const { userId, firstname, lastname, birthday, color } = req.body
+  console.log(userId, firstname, lastname, birthday, color)
 })
 
 module.exports = router
