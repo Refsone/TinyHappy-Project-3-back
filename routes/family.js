3a44f559b8ea7af5cd02fee6a319364897174b07
jerkodeur <jerome.potie@gmail.com>
2020-06-18 11:56:14 +0200

    Adjust params

--- routes/family.js
+++ routes/family.js
@@ -9,10 +9,11 @@ const validateRequest = SchemaValidator(true)
 
 router.post('/', validateRequest, (req, res) => {
   const formData = req.body
+  console.log(formData)
 
   connection.query('INSERT INTO family_member SET ?', formData, (err, results) => {
     if (err) {
-      res.status(400).send('An error is occured when created a new member')
+      res.status(400).send('An error is occured when created a new member' + err)
     } else {
       console.log('req ok')
       res.status(201).send('New family user created')
--- schemas.js
+++ schemas.js
@@ -2,14 +2,14 @@ const Joi = require('joi')
 
 const id = Joi.number().integer().min(1).required()
 const nameNotRequired = Joi.string().min(2).max(30).allow('')
-const nameRequired = Joi.string().min(3).max(30).required()
-const birthdayDate = Joi.date().greater('1-1-1920').less('now').allow('')
+const nameRequired = Joi.string().min(2).max(30).required()
+const birthdayDate = Joi.date().greater('1-1-1920').less('now').allow('', null)
 const color = Joi.number().integer().min(1).max(1000).required('')
 
 const familyAddSchema = Joi.object().keys({
   user_id: id,
-  family_firstname: nameNotRequired,
-  family_lastname: nameRequired,
+  family_firstname: nameRequired,
+  family_lastname: nameNotRequired,
   family_surname: nameNotRequired,
   family_birthday: birthdayDate,
   color_family_id: color
