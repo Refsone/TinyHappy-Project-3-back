75e95d74bd0aa9f627b36a0859e2307e5b52ec4a
Moni <monica_be@hotmail.fr>
2020-06-30 01:22:50 +0200

    adding getUser

--- routes/login.js
+++ routes/login.js
@@ -4,17 +4,17 @@ const connection = require('../conf')
 
 router.post('/', (req, res) => {
   const formData = req.body
-  res.send('Register')
-  console.log(formData)
 
-  connection.query('INSERT INTO user SET ?', formData, (err, results) => {
-    if (err) {
-      res.status(400).send('An error is occured when created a new member' + err)
-    } else {
-      console.log('req ok')
-      res.status(201).send('New family user created')
-    }
-  })
+  const getUser = (userMail) => {
+    connection.query('INSERT INTO user SET ?', formData, [userMail], (err, results) => {
+      if (err) {
+        res.status(400).send('Error while connecting to DB' + err)
+      } else {
+        console.log('req is ok')
+        res.status(201).send('You are connected')
+      }
+    })
+  }
 })
 
 module.exports = router
