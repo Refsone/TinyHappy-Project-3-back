75e80b1ddc2d1a029a20a360c532f3d08f22846d
Auxence <auxence.blondel@gmail.com>
2020-06-30 15:16:17 +0200

    add route share and nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const colors = require('./colors')
 const family = require('./family')
 const moments = require('./moments')
+const share = require('./share')
 const users = require('./users')
 
 module.exports = {
   colors,
   family,
   moments,
+  share,
   users
 }
--- routes/share.js
+++ routes/share.js
@@ -1,27 +1,13 @@
-require('dotenv').config()
-const cors = require('cors')
 const express = require('express')
-const app = express()
-const routes = require('./routes/index')
 
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+const router = express.Router()
+// const connection = require('../conf')
 
-app.use(cors('*'))
-
-app.use('/colors', routes.colors)
-app.use('/family', routes.family)
-app.use('/moments', routes.moments)
-app.use('/users', routes.users)
-
-app.post('/share', (req, res) => {
+router.post('/', (req, res) => {
 
   const mailOutput = `
-  You have an email
-  ${req.body}
-  `
+    You have an email
+    `
   // let testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
@@ -33,7 +19,7 @@ app.post('/share', (req, res) => {
       user: 'auxence_6033@hotmail.fr', // generated ethereal user
       pass: '.33Girondin59', // generated ethereal password
     },
-  });
+  })
 
   // send mail with defined transport object
   let info = transporter.sendMail({
@@ -42,7 +28,7 @@ app.post('/share', (req, res) => {
     subject: 'Hello ✔', // Subject line
     text: 'Hello world?', // plain text body
     html: mailOutput, // html body
-  });
+  })
 
   console.log('Message sent: %s', info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
@@ -50,12 +36,6 @@ app.post('/share', (req, res) => {
   // Preview only available when sending through an Ethereal account
   console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-})
 
-app.listen(process.env.PORT, (err) => {
-  if (err) {
-    console.log('Something bad happened...')
-  } else {
-    console.log(`server is listening on port ${process.env.PORT}`)
-  }
+  module.exports = router
 })
