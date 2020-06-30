11e0de3ff8851de22ff278778405709a8ba97e86
Moni <monica_be@hotmail.fr>
2020-06-30 19:01:33 +0200

    ferif regex email

--- routes/services/verif.service.js
+++ routes/services/verif.service.js
@@ -1,5 +1,5 @@
 const verifyEmail = (req, res, next) => {
-  const emailRegEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+  const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
   if (!emailRegEx.test(req.body.userMail)) {
     return res.status(401).send('Unauthorized user')
   }
