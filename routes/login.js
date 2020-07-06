ae97a31534f9ae212da5665a2711c19dd4ca137f
moni-be <monica_be@hotmail.fr>
2020-07-06 17:02:21 +0200

    Update login.js
    no console log !!

--- routes/login.js
+++ routes/login.js
@@ -10,20 +10,15 @@ const { verifyEmail } = require('./services/verif.service')
 const checkingtUser = (req, res, next) => {
   const userMail = req.body.user_mail
   const userPassword = req.body.user_password
-  console.log(userMail)
-  console.log(userPassword)
-
   connection.query('SELECT * FROM user WHERE user_mail = ?', userMail, (err, result) => {
     if (err) {
       res.status(500).send('Error while connecting to DB' + err)
     } else if (!result[0]) {
-      console.log('req is ok')
       return res.status(409).send('unknown user')
     }
     console.log(result[0])
     const pwdIsValid = bcrypt.compareSync(userPassword, result[0].user_password)
     if (!pwdIsValid) {
-      console.log(pwdIsValid)
       return res.status(401).send({ auth: false, token: null })
     }
     req.user = result[0]
