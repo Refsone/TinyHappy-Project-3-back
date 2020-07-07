64aa69f0e6972c23ee05525636d0bf3e3af578cf
Jerôme Potié <49954831+jerkodeur@users.noreply.github.com>
2020-07-07 14:54:07 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const colors = require('./colors')
 const contacts = require('./contacts')
 const familyMembers = require('./family-members')
+const login = require('./login')
 const moments = require('./moments')
 const share = require('./share')
-const login = require('./login')
 const signUp = require('./signUp')
 const users = require('./users')
 
@@ -11,9 +11,9 @@ module.exports = {
   colors,
   contacts,
   familyMembers,
-  moments,
-  users,
   login,
+  moments,
   share,
-  signUp
+  signUp,
+  users
 }
