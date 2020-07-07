c6bed1a585d68c46d69b722d21fc844f637570d2
moni-be <monica_be@hotmail.fr>
2020-07-07 15:00:41 +0200

    Update verif.service.js
    no console logs

--- routes/services/verif.service.js
+++ routes/services/verif.service.js
@@ -1,5 +1,4 @@
 const verifyEmail = (req, res, next) => {
-  console.log(req.body)
   const emailRegEx = /^([a-zA-Z0-9-.]+)@([a-zA-Z0-9-.]+).([a-zA-Z]{2,5})$/
   if (!emailRegEx.test(req.body.user_mail)) {
     return res.status(401).send('Unauthorized user')
