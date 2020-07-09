14b0cb6518c3eae6f8d0f924c319143d9bab6750
Jerôme Potié <49954831+jerkodeur@users.noreply.github.com>
2020-07-09 21:46:44 +0200

    Update verif.service.js

--- service/verif.service.js
+++ service/verif.service.js
@@ -4,7 +4,6 @@ const { secret } = require('../conf')
 const verifyEmail = (req, res, next) => {
   const emailRegEx = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/
   if (!emailRegEx.test(req.body.user_mail)) {
-    console.log('regex')
     return res.status(401).send('Unauthorized user!')
   }
   next()
