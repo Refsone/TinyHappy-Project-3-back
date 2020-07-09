a1cca6e272524cf6a8b95c1117fa696933d3feea
moni-be <monica_be@hotmail.fr>
2020-07-09 21:45:45 +0200

    Update mailing.js
    no console.log

--- routes/mailing.js
+++ routes/mailing.js
@@ -5,7 +5,6 @@ const { verifyEmail, verifyIfEmailExist } = require('../service/verif.service')
 const sendTempPasswordByMail = require('../service/sendMailByMailJet')
 
 router.post('/tempPassword', verifyEmail, verifyIfEmailExist, sendTempPasswordByMail, (req, res) => {
-  console.log('mail is send !')
   return res.status(200).send('The new password is send')
 })
 
