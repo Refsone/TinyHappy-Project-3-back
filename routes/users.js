e6f5123800b60eb6d8fbbc04bc0524f17220058d
AuxB <auxence.blondel@gmail.com>
2020-07-09 16:32:48 +0200

    simplify sql request moment

--- routes/users.js
+++ routes/users.js
@@ -51,7 +51,17 @@ router.get('/:user_id/family-members/:member_id', (req, res) => {
 })
 
 router.get('/:id/moments', (req, res) => {
-  connection.query('SELECT moment.id, moment_text, moment_context, moment_favorite, moment_event_date, family_firstname, type, user_isPresent, color FROM moment JOIN family_moment ON moment_id=moment.id JOIN family_member fme ON family_member_id=fme.id JOIN color_family ON color_family_id=color_family.id JOIN moment_type ON moment.moment_type_id=moment_type.id WHERE moment.user_id = ? GROUP BY moment.id, fme.id ORDER BY moment.id ASC', [req.params.id], (err, results) => {
+  const sql = `
+  SELECT moment.id, moment_text, moment_context, moment_favorite, moment_event_date, family_firstname, type, user_isPresent, color FROM moment
+  JOIN family_moment ON moment_id=moment.id
+  JOIN family_member fme ON family_member_id=fme.id
+  JOIN color_family ON color_family_id=color_family.id
+  JOIN moment_type ON moment.moment_type_id=moment_type.id
+  WHERE moment.user_id = ?
+  GROUP BY moment.id, fme.id
+  ORDER BY moment.id ASC
+  `
+  connection.query(sql, [req.params.id], (err, results) => {
     if (err) {
       res.status(500).send('Erreur lors de la récupération des moments')
     } else {
