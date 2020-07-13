14c1862681979c6ea6eae3950558ca674cb8f416
hipro90 <hipro90@gmail.com>
2020-07-13 19:19:34 +0200

    ok

--- routes/users.js
+++ routes/users.js
@@ -180,14 +180,15 @@ router.put('/:id/modify-password', verifyToken, (req, res) => {
   const id = req.params.id
   const newPassword = req.body.newPassword
   const user_password = req.body.actualPassword
-  connection.query('SELECT user_password FROM user WHERE id = ?', id, (err, result) => {
-    const pwdIsValid = bcrypt.compareSync(user_password, result[0].user_password)
+  connection.query('SELECT user_password FROM user WHERE id = ?', id, (err, result) => {    
+    // const pwdIsValid = bcrypt.compareSync(user_password, result[0].user_password)
     if (err) {
+      console.log(err)
       return res.status(500).json({
         message: err.message,
         sql: err.sql
       })
-    } else if (pwdIsValid) {
+    } else if (bcrypt.compareSync(user_password, result[0].user_password)) {
       const hashNewPassword = bcrypt.hashSync(newPassword)
       connection.query('UPDATE user SET user_password = ? WHERE id = ?', [hashNewPassword, id], (err, result) => {
         if (err) {
@@ -196,11 +197,11 @@ router.put('/:id/modify-password', verifyToken, (req, res) => {
             sql: err.sql
           })
         } else {
-          res.status(201).json('password change successfully')
+          res.sendStatus(201)
         }
       })
     } else {
-      res.sendStatus(400)
+      res.sendStatus(401)
     }
   }
   )
